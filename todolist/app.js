f9c6db99af6594028333cd59853df3df44f0e2da
julienwalles <j.walles@cci-formation.nc>
2021-04-01 10:49:22 +1100

    todo list

--- todolist/app.js
+++ todolist/app.js
@@ -1,14 +1,17 @@
 window.onload = () => {
+
   const form1 = document.querySelector('#addForm1'); // variable pour le form1
   const form2 = document.querySelector('#addForm2'); //variable pour le form2
+
 
   let items = document.getElementById('items'); // variable pour form2
   let submit = document.getElementById('submit'); // variable pour btn submit
 
-  let editItem = null;
 
   form1.addEventListener('submit', addItem);
   items.addEventListener('click', removeItem);
+
+
 };
 
 function addItem(e) {
@@ -33,53 +36,102 @@ function addItem(e) {
   }
 
   document.getElementById("item").value = ""; // vider le champ input
-  
-    // var span = document.createElement("span");
-  
-    // var edit_txt = document.createTextNode("Edit");
-  
-    // span.className = "close";
-  
-    // span.appendChild(edit_txt);
-  
-    // newLi.appendChild(span);
 
-    var span = document.createElement("span");
-  
-    var edit_txt = document.createElement("button");
-  
-    span.className = "edit";
-  
-    span.append(edit_txt);
-  
-    newLi.appendChild(span);
+  var span = document.createElement("span");
 
-    var span2 = document.createElement("span");
-  
-    var edit2_txt = document.createElement("button");
-  
-    span2.className = "delete";
-  
-    span2.append(edit2_txt);
-  
-    newLi.appendChild(span2);
+  // var edit_txt = document.createTextNode("\ud83d\uddd1");
+  var edit_txt = document.createElement("button");
+
+  edit_txt.textContent = "Delete";
+
+  span.className = "delete";
+  span.id = "Btndelete";
+
+  span.append(edit_txt);
+
+  newLi.appendChild(span);
+
+  var span2 = document.createElement("span");
+
+  // var edit2_txt = document.createTextNode("\u270E");
+  var edit2_txt = document.createElement("button");
+
+  edit2_txt.textContent = "Edit";
+
+  span2.className = "edit";
+
+  span2.id = "Btnedit";
+
+  span2.append(edit2_txt);
+
+  newLi.appendChild(span2);
+
+
+  //éditer un item
+
+  var itemEdit = document.getElementsByTagName("li");
+
+  for (i = 0; i < itemEdit.length; i++) {
+
+    itemEdit[i].onclick = function () {
+
+      var inu = this.innerText;
+
+      document.getElementById("item").value = inu;
+
+      submit.value = "Edit";
+
+    }
   }
+}
+//remettre l'item édité à sa place
 
-  
+
+
+
+
 
 
 
 
 function removeItem(e) {
   e.preventDefault();
-  /**
-   *
-   * a vous de jouer
-   */
+
+  //supprimer des items
+
+  var list = document.getElementsByClassName("delete");
+
+  for (i = 0; i < list.length; i++) {
+
+    list[i].onclick = function () {
+
+      var del = this.parentElement;
+
+      del.style.display = "none";
+    }
+  }
+
 }
 
 function toggleButton(ref, btnID) {
 
+}
 
+document.getElementById("edit").addEventListener("click,", edition);
 
+function edition() {
+
+  var itemEdit = document.getElementsByTagName("li");
+
+  for (i = 0; i < itemEdit.length; i++) {
+
+    itemEdit[i].onclick = function () {
+
+      var inu = this.innerText;
+
+      document.getElementById("item").value = inu;
+
+      submit.value = "Edit";
+    }
+  }
 }
